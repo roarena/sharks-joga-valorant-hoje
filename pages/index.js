1ad5b652231208f233593d1dfd63ccabf431690a
Rodrigo Câmara <rodrigoarenacamara@gmail.com>
2021-05-25 01:35:01 -0300

    Fixing typos
    Signed-off-by: Rodrigo Câmara <rodrigoarenacamara@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -58,15 +58,15 @@ export default function Home(props) {
                     alt={"Sharks Logo"}
                 />
             </center>
-            <header class="w3-panel w3-center w3-opacity">
+            <header className="w3-panel w3-center w3-opacity">
                 <h1>A Sharks joga hoje?</h1>
                 {playToday(props.data.play_today)}
                 <h2>{teamLogo(props, 0)} <a href={"https://liquipedia.net" + props.data.links[0]} target="_blank">{props.data.teams[0]}</a> vs <a href={"https://liquipedia.net" + props.data.links[1]} target="_blank">{props.data.teams[1]}</a> {teamLogo(props, 1)}</h2>
                 Próximo jogo: <u>{props.data.date[0]}</u> aproximadamente às <u>{getMatchTime(props.data.time[0])}</u><br></br>
                 Informações obtidas no <a href="https://liquipedia.net">Liquipedia</a>
                 <p>by Rodrigo "Inkky" Câmara</p>
-                <a class="github-button" href="https://github.com/roarena/sharks-joga-valorant-hoje" data-size="large" data-show-count="true" aria-label="roarena/sharks-joga-valorant-hoje on GitHub">GitHub</a>
-                <p><a href="https://twitter.com/_rodrigocamara?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @_rodrigocamara</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></p>
+                <a className="github-button" href="https://github.com/roarena/sharks-joga-valorant-hoje" data-size="large" data-show-count="true" aria-label="roarena/sharks-joga-valorant-hoje on GitHub">GitHub</a>
+                <p><a href="https://twitter.com/_rodrigocamara?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">Follow @_rodrigocamara</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script></p>
             </header>
         </div>
     )
